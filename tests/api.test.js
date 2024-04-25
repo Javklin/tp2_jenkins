56d08155f6733545dc5321113a3b46f77b385b77
Javklin <allvis.gautier@gmail.com>
2024-04-25 10:43:34 +0200

    jest force exit

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,10 +3,34 @@ const request = require('supertest');
 const app = require('../backend/server');  // Assurez-vous que cela pointe vers votre fichier serveur correctement configuré
 
 describe('API Tests', () => {
+  let server;
+
+  beforeAll((done) => {
+    // Start the server and capture the server object
+    server = app.listen(3000, () => {
+      console.log('Server started on port 3000');
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    // Close the server
+    server.close((err) => {
+      if (err) {
+        console.error('Error closing server:', err);
+        done(err);
+      } else {
+        console.log('Server closed');
+        done();
+      }
+    });
+  });
+
   it('GET /contacts - should return all contacts', async () => {
     const response = await request(app).get('/contacts');
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeInstanceOf(Array);
+ 
   });
 
   /*
